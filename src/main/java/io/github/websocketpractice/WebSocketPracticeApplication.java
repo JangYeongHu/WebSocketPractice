f0baec453bb12261ac65a71d94a3aaef13887ebd
JangYeongHu <jyhoo040912@gmail.com>
2025-07-18 19:52:11 +0900

    초기 커밋

---
+++ src/main/java/io/github/websocketpractice/WebSocketPracticeApplication.java
@@ -0,0 +1,13 @@
+package io.github.websocketpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/websocketpractice/WebSocketPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.websocketpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
